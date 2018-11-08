bd675850e45652c726f26be2847332ac0519c49a
Pavel Kyurkchiev <pavkyu@gmail.com>
2018-11-08 22:38:28 +0200

    week 4

---
+++ exercises/week_4/MC.Website/MC.Data/Entities/Rating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MC.Data.Entities
+{
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Rating")]
+        [Required]
+        [StringLength(5)]
+        public string RatingValue { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
