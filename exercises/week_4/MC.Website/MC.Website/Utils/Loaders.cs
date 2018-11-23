6de587e18bca444619a262abd8a99bb609427c21
Pavel Kyurkchiev <pavkyu@gmail.com>
2018-11-23 22:35:53 +0200

    week 4

---
+++ exercises/week_4/MC.Website/MC.Website/Utils/Loaders.cs
@@ -0,0 +1,19 @@
+﻿using MC.Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MC.Website.Utils
+{
+    public static class Loaders
+    {
+        private static ApplicationDbContext dbApp = new ApplicationDbContext();
+
+        public static IEnumerable<SelectListItem> RolesToSelectItem()
+        {
+            return dbApp.Roles.Select(x => new SelectListItem { Value = x.Name, Text = x.Name });
+        }
+    }
+}
