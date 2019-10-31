764477ec75eb553e0dbdca2b8fd59bfb992268fa
Pavel Kyurkchiev <pavkyu@gmail.com>
2019-10-31 20:42:16 +0200

    week 3

---
+++ exercises/week_3/MC.Website/MC.Data/Migrations/201710081124394_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace MC.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 150),
+                        ReleaseDate = c.DateTime(),
+                        Country = c.String(maxLength: 300),
+                        Description = c.String(maxLength: 4000),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
