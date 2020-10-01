74fcfa74044a68a43bd0a4b47377b1690d9a1638
Pavel Kyurkchiev <pavkyu@gmail.com>
2020-10-01 19:31:03 +0300

    week 1

---
+++ exercises/week_1/MC.Website/MC.Data/Context/MovieCatalogDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MC.Data.Entities;
+using System.Data.Entity;
+
+namespace MC.Data.Context
+{
+    public class MovieCatalogDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
