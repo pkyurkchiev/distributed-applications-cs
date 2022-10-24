8fceba336f1278e99b5c23f54d7894b1164d1240
Pavel Kyurkchiev <pavkyu@gmail.com>
2022-10-24 10:50:35 +0300

    week 3

---
+++ exercises/net_6/week_3/MovieCatalog/MC.Website/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using MC.Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MC.Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Hello([FromRoute] int? id, [FromQuery] string name)
+        {
+            ViewBag.Id = id;
+            ViewBag.Name = name;
+            return View();
+        }
+
+        public IActionResult Calculator([FromQuery] double? a, [FromQuery] double? b)
+        {
+            double? perimeter, aria;
+            perimeter = 2 * a + 2 * b;
+            aria = a * b;
+
+            ViewBag.A = a;
+            ViewBag.B = b;
+            ViewBag.Perimeter = perimeter;
+            ViewBag.Aria = aria;
+
+            return View();
+        }
+    }
+}
---
+++ exercises/net_6/week_3/MovieCatalog/MC.Website/Controllers/WritersController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MC.Data.Entities;
+using MC.Website.Data;
+
+namespace MC.Website.Controllers
+{
+    public class WritersController : Controller
+    {
+        private readonly MovieCatalogDbContext _context;
+
+        public WritersController(MovieCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Writers
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Writers.ToListAsync());
+        }
+
+        // GET: Writers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Writers == null)
+            {
+                return NotFound();
+            }
+
+            var writer = await _context.Writers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (writer == null)
+            {
+                return NotFound();
+            }
+
+            return View(writer);
+        }
+
+        // GET: Writers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Writers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,UserName")] Writer writer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(writer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(writer);
+        }
+
+        // GET: Writers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Writers == null)
+            {
+                return NotFound();
+            }
+
+            var writer = await _context.Writers.FindAsync(id);
+            if (writer == null)
+            {
+                return NotFound();
+            }
+            return View(writer);
+        }
+
+        // POST: Writers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,UserName")] Writer writer)
+        {
+            if (id != writer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(writer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WriterExists(writer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(writer);
+        }
+
+        // GET: Writers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Writers == null)
+            {
+                return NotFound();
+            }
+
+            var writer = await _context.Writers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (writer == null)
+            {
+                return NotFound();
+            }
+
+            return View(writer);
+        }
+
+        // POST: Writers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Writers == null)
+            {
+                return Problem("Entity set 'MovieCatalogDbContext.Writers'  is null.");
+            }
+            var writer = await _context.Writers.FindAsync(id);
+            if (writer != null)
+            {
+                _context.Writers.Remove(writer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WriterExists(int id)
+        {
+          return _context.Writers.Any(e => e.Id == id);
+        }
+    }
+}
