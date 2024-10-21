0ba98e690bc0a52ee50b9a995feca13fe9294934
Pavel Kyurkchiev <pavkyu@gmail.com>
2024-10-21 12:21:32 +0300

    day 2

---
+++ exercises/01/MovieCatalog/MC.Website/Data/MovieCatalogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using MC.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MC.Website.Data
+{
+    public class MovieCatalogDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieCatalogDbContext(DbContextOptions<MovieCatalogDbContext> options) : base(options)
+        {
+        }
+    }
+}
